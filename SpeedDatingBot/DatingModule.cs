3d73d7130b4a52ca2b3a86f953c875290c70a41b
Nathaniel Sneddon <nathaniel@sneddonfamily.com>
2021-02-15 21:03:03 -0800

    Swapping is done

--- SpeedDatingBot/DatingModule.cs
+++ SpeedDatingBot/DatingModule.cs
@@ -18,12 +18,14 @@ namespace SpeedDatingBot
         private DiscordSocketClient _client;
         private DatingSession _session;
         private ulong _id;
+        private string _name;
 
         public DatingModule(DiscordSocketClient client, DatingSession session)
         {
             _client = client;
             _session = session;
             _id = 810729843280183316;
+            _name = $"Breakout room ";
         }
 
         [Command("startdating", RunMode = RunMode.Async)]
@@ -73,16 +75,10 @@ namespace SpeedDatingBot
                     continue;
                 }
 
-                var overwrites = new List<Overwrite>();
-                overwrites.Add(new Overwrite(user.Id, PermissionTarget.User,
-                    new OverwritePermissions(
-                        connect: PermValue.Allow,
-                        speak: PermValue.Allow,
-                        useVoiceActivation: PermValue.Allow
-                    )));
+                Console.WriteLine($"Channel Index is {channelIndex}");
 
-                IVoiceChannel newChannel =
-                    await FindOrCreateVoiceChannelAsync(channelIndex, user, datingCategory.Id);
+                IVoiceChannel newChannel = await FindOrCreateVoiceChannelAsync(channelIndex, user, datingCategory.Id);
+                await newChannel.AddPermissionOverwriteAsync(user, Overwrites.ConnectVoice);
                 await user.ModifyAsync(prop => prop.Channel = Optional.Create(newChannel));
             }
         }
@@ -111,10 +107,41 @@ namespace SpeedDatingBot
             _session.EndDatingSession();
         }
 
+        [Command("swap", RunMode = RunMode.Async)]
+        [Summary("Swap people to another room")]
+        public async Task SwapRooms()
+        {
+            var datingCategory = Context.Guild.GetCategoryChannel(_session.DatingCategoryId);
+            var channels = from channel in datingCategory.Channels
+                orderby channel.Position
+                select channel as SocketVoiceChannel;
+            List<SocketVoiceChannel> socketVoiceChannels = new List<SocketVoiceChannel>();
+            foreach (var channel in channels)
+            {
+                socketVoiceChannels.Add(channel);
+            }
+
+            // IVoiceChannel[] socketVoiceChannels = channels as SocketVoiceChannel[] ?? list.ToArray();
+            ulong previousUserId = 0;
+            int count = socketVoiceChannels.Count;
+            for (int i = 0; i < count; i++)
+            {
+                IGuildUser toBeMoved = socketVoiceChannels[i].Users
+                    .FirstOrDefault(x => x.Roles.Any(y => y.Name == "Boy") && x.Id != previousUserId);
+                if (toBeMoved == null) continue;
+                previousUserId = toBeMoved.Id;
+                SocketVoiceChannel newChannel = socketVoiceChannels[(i + 1) % count];
+                await newChannel.AddPermissionOverwriteAsync(toBeMoved, Overwrites.ConnectVoice);
+                await socketVoiceChannels[i].RemovePermissionOverwriteAsync(toBeMoved);
+                await toBeMoved.ModifyAsync(user => user.Channel = newChannel);
+            }
+        }
+
         private async Task<IVoiceChannel> FindOrCreateVoiceChannelAsync(int offset, IGuildUser user,
             ulong voiceCategoryId)
         {
-            string name = $"Breakout room {offset}";
+            string name = _name + offset;
+            Console.WriteLine(name);
             IVoiceChannel voiceChannel = (IVoiceChannel) Context.Guild.Channels.FirstOrDefault(x => x.Name == name) ??
                                          await Context.Guild.CreateVoiceChannelAsync(name,
                                              x => x.CategoryId = voiceCategoryId);
